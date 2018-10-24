2aa879f397eba4e5f5ed043fd362462294eef298
DC Barringer <nukdcbear@gmail.com>
2018-10-24 12:24:07 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,3 +6,9 @@ node 'master.puppet.vm' {
 node 'tdcos7pupm02.eastus2.cloudapp.azure.com' {
   include role::master_server
 }
+node /^web/ {
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
+}
