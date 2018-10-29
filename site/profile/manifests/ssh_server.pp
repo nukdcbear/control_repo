4c9b6c2f73716432cd3fd1cfd50114af19b73b2a
DC Barringer <nukdcbear@gmail.com>
2018-10-29 15:41:20 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDNUii2Mz0w5qp/DTPJj0KP7eodv2UgLD4aDmiSLE4V54Xk13GsoJyQGohyAR/ik9R3EtWXSLG3uDGkdakjCgy/VfKv7q5n3aDMFBws1Tuq/RO//a41bfJ5YpgLPaNJxSj9UpbEYDgIlxBMX7FcbX2A42by67jTM6Uqujcr8XmvduJWOa2bThcmsdnKCnUTJZhGydr8DVygSE5f5jw3HOfGBRmSMvxPUOqkBeZizMznsSbR9GMz62Aqv0KjdTsPWtp62M/zDe5wE4HC1m+5CJ0KqEF4qM+cTncyuaL1UHCROkrysN7uLV+YPiNxmRKuaaWBwvqkXf4cXy2+EqlovJSZ',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/bRXi8HiJPmFaum/atkDN05JUYIdSM1Hd87gSi7mg/ty/Lo+jQhkQJkiSjeW4X5bzPC8NUdDGXZV9Z89j56bWVwMaJ/aAxoej2ZVqW//WFg433nzG6jeXDUYOg7M/2T5qufDcVsl9kFe/BDr+KGVwmfOFsKScmwLiqKn1YWywxSlqJ9e0Lzt8dnTEh0yWZFjBfZeDjTXMYSHY6Qb3dNOIfzTOlBf6Q10R5GPS54UgkjBCdjDCvvXfLb4crRjyWYFcGH8KoPYbvytCY3cEZUAu7bYeST0rwy3o0xPFUbnL6qIzo4f6+hZ3YkKy3bQQFpF0tDbS+EanUBvT3QSQyMBb',
 	}  
 }
